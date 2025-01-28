138505ed50c0f01a1ce33a21229cc9e625af576d
youngroma <krivichroma228@gmail.com>
2025-01-28 20:13:35 +0100

    removed help_text

--- quizes/models.py
+++ quizes/models.py
@@ -9,11 +9,11 @@ DIFF_CHOICES = (
 
 
 class Quiz(models.Model):
-    title = models.CharField(max_length=255, help_text="Title of the quiz", blank=True)
-    description = models.TextField(help_text="Description of the quiz", blank=True, null=True)
-    time = models.IntegerField(help_text="Duration of the quiz in minutes")
-    required_score = models.DecimalField(help_text="Score needed to pass the quiz in %", decimal_places=2, max_digits=6)
-    difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES, help_text="Difficulty level of the quiz")
+    title = models.CharField(max_length=64, blank=True)
+    description = models.CharField(max_length=256, blank=True, null=True)
+    time = models.IntegerField()
+    required_score = models.DecimalField(decimal_places=2, max_digits=6)
+    difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES)
 
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
